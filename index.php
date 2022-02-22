1d070d84bd049f9a4dfcaea322256bc0e1fd5311
Fozan Bebe Moreno <fozbebemoreno@gmail.com>
2022-02-22 14:36:02 +0700

    menambah list baru

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, assumenda animi? Voluptatum eius omnis consectetur et culpa inventore debitis? Molestiae libero totam architecto eum magnam itaque aspernatur, inventore animi quos.</p>
+    <ul>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+    </ul>
 </body>
 
 </html>
